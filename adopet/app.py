90243cdb56df140a0f7a6313b1b64a9f27fb93a3
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-04-01 08:23:33 -0300

    feat: inject db on app

--- adopet/app.py
+++ adopet/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+from adopet.ext.database import model
 
-def init_app():
+
+def init_app() -> Flask:
     app = Flask(__name__)
+    database = model.init_app(app=app)
+
+    return app
