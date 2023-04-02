8084eed88acd10373d7e9b9c86b793911cc57da3
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-04-02 10:25:12 -0300

    feat: init db an create tables

--- adopet/app.py
+++ adopet/app.py
@@ -2,12 +2,10 @@ from dynaconf import FlaskDynaconf
 from flask import Flask
 
 from adopet.ext.database import database
-from adopet.ext.database.database import create_db
 
 
 def init_app() -> Flask:
     app = Flask(__name__)
     FlaskDynaconf(app)
     database.init_app(app=app)
-    create_db(app=app)
     return app
