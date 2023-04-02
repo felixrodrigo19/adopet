03a8a96e8fe6f4504638de8860f8288f8dfe10e0
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-04-02 09:22:04 -0300

    feat: setup config

--- adopet/app.py
+++ adopet/app.py
@@ -1,3 +1,4 @@
+from dynaconf import FlaskDynaconf
 from flask import Flask
 
 from adopet.ext.database import database
@@ -6,6 +7,7 @@ from adopet.ext.database.database import create_db
 
 def init_app() -> Flask:
     app = Flask(__name__)
+    FlaskDynaconf(app)
     database.init_app(app=app)
     create_db(connection='postgresql://user:password@host:port/dbname', dbname="dbname", app=app)
     return app
